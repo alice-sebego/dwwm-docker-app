337c3e97767b3d737352faf4ac1e07b13c997f61
Alice Sebego <alice.etvotrecom@gmail.com>
2022-11-16 16:58:57 +0100

    install assets and organize front files

---
+++ app/header.php
@@ -0,0 +1,26 @@
+<header>
+    <section class="navigation">
+        <div class="nav-content">
+            <div class="logo">
+                <a href="#">
+                    <img src="https://cdn.svgporn.com/logos/storyblocks.svg" alt="Storyblocks">
+                </a>
+            </div>
+            <nav class="menu-header">
+              <ul class="nav-top">
+                <li class="category">Category 1</li>
+                <li class="category">Category 2</li>
+                <li class="category">Category 3</li>
+                <li class="category">Category 4</li>
+              </ul>  
+            </nav>
+            <div class="search-box">
+                <form action="" method="get">
+                    <input type="text" name="" id="" placeholder="Search our blog ...">
+                    <input type="submit" value="">
+                </form>
+            </div>
+
+        </div>
+    </section>
+</header>
--- app/index.php
+++ app/index.php
@@ -1,20 +1,19 @@
 <?php
-//These are the defined authentication environment in the db service
-
-// The MySQL service named in the docker-compose.yml.
-$host = 'db';
-
-// Database use name
-$user = 'MYSQL_USER';
-
-//database user password
-$pass = 'MYSQL_PASSWORD';
-
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
+include_once "inc/connexion.php";
+$title = "Home | Welcome on Storyblocks";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once "inc/head.php"; ?>
+<body>
+    <?php include_once "header.php"; ?>
+    <section class="main-content">
+        <main>
+            <div class="banner-content">
+                <h1>All Blog Articles</h1>
+                <p>Welcome to our library full of helpful resources from image and video editing to how to be a successful video marketer.</p>
+            </div>
+        </main>
+    </section>
+</body>
+</html>
