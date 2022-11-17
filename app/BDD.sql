997fa362791296ebf7f0b55d2c62846ad58fda99
Alice Sebego <alice.etvotrecom@gmail.com>
2022-11-17 12:22:33 +0100

    change port of php-apache and phpmyadminimages and add type of engine for sql file

--- app/BDD.sql
+++ app/BDD.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` VARCHAR(256) NOT NULL,
   `picture` VARCHAR(75),
   `role` INT(1),
-);
+)ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `article` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,18 +14,20 @@ CREATE TABLE IF NOT EXISTS `article` (
   `title` VARCHAR(80) NOT NULL,
   `content` TEXT NOT NULL,
   `published_at` DATETIME NOT NULL,
-  `updated_at` DATETIME
+  `updated_at` DATETIME,
+  `illustration` VARCHAR(75),
+  `notation` INT NOT NULL
 
   FOREIGN KEY(author) REFERENCES users(id)
-);
+)ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `article_category` (
   `article_id` INTEGER,
   `category_id` INTEGER,
   PRIMARY KEY(article_id, category_id)
-);
+)ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `category` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `name` VARCHAR(30) NOT NULL UNIQUE
-);
+)ENGINE = InnoDB;
